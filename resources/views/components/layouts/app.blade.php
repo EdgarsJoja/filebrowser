a81af3102eff80e1f8749040a5baf18a024b2bb8
Edgars Joja <edgarsjoja@gmail.com>
2024-06-30 19:13:43 +0300

    Add favicon

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -1,9 +1,11 @@
 <html>
     <head lang="en">
-        <title>{{ $title ?? 'FileBrowser' }}</title>
+        <title>{{ $title ?? 'File Browser' }}</title>
 
         @vite('resources/css/app.css')
         @vite('resources/js/app.js')
+
+        <link rel="icon" type="image/x-icon" href="favicon.png">
     </head>
     <body>
         <div class="px-8 max-w-screen-md mx-auto">
