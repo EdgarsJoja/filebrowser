412b50e97971acb56e5bc16b43e2e387ec432642
Edgars Joja <edgarsjoja@gmail.com>
2024-07-23 23:09:43 +0300

    Implement maryUI modals

--- app/Livewire/Menu.php
+++ app/Livewire/Menu.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Livewire;
 
 use Illuminate\Contracts\View\View;
+use Livewire\Attributes\On;
 use Livewire\Attributes\Reactive;
 use Livewire\Component;
 
@@ -13,8 +14,19 @@ class Menu extends Component
     #[Reactive]
     public string $currentDirectory = '';
 
+    public bool $fileUploadModal = false;
+    public bool $createDirectoryModal = false;
+
     public function render(): View
     {
         return view('livewire.menu');
+    }
+
+    #[On('file-uploaded')]
+    #[On('directory-created')]
+    public function closeModals(): void
+    {
+        $this->fileUploadModal = false;
+        $this->createDirectoryModal = false;
     }
 }
--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -1,39 +1,11 @@
 <div class="flex flex-col sm:flex-row gap-4 sm:items-center">
-    <div x-data="{ open: false }" @file-uploaded.window="open = false">
-        <x-button label="Upload Files" icon="o-arrow-up-tray" @click="open = true" class="btn-primary" />
-        <div
-            x-show="open"
-            x-transition.opacity
-            x-cloak
-            class="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-neutral/20 backdrop-blur-sm duration-300"
-        >
-            <div
-                @click.outside="open = false"
-                class="relative m-4 w-full min-w-[40%] max-w-[80%] lg:max-w-[600px] rounded-lg bg-base-200 font-sans text-base font-light leading-relaxed antialiased shadow-lg"
-            >
-                <div class="p-8">
-                    <livewire:file-upload :currentDirectory="$currentDirectory" />
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-modal wire:model="fileUploadModal" class="backdrop-blur">
+        <livewire:file-upload :currentDirectory="$currentDirectory" />
+    </x-modal>
+    <x-button label="Upload Files" icon="o-arrow-up-tray" @click="$wire.fileUploadModal = true" class="btn-primary" />
 
-    <div x-data="{ open: false }" @directory-created.window="open = false">
-        <x-button label="Create Folder" icon="o-folder-plus" @click="open = true" />
-        <div
-            x-show="open"
-            x-transition.opacity
-            x-cloak
-            class="fixed inset-0 z-[999] grid h-screen w-screen place-items-center bg-neutral/20 backdrop-blur-sm duration-300"
-        >
-            <div
-                @click.outside="open = false"
-                class="relative m-4 w-full min-w-[40%] max-w-[80%] lg:max-w-[600px] rounded-lg font-sans text-base font-light leading-relaxed antialiased shadow-lg bg-base-200"
-            >
-                <div class="p-8">
-                    <livewire:create-folder :currentDirectory="$currentDirectory" />
-                </div>
-            </div>
-        </div>
-    </div>
+    <x-modal wire:model="createDirectoryModal" class="backdrop-blur">
+        <livewire:create-folder :currentDirectory="$currentDirectory" />
+    </x-modal>
+    <x-button label="Create Folder" icon="o-folder-plus" @click="$wire.createDirectoryModal = true" />
 </div>
