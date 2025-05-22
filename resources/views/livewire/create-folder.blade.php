30486e833835d2d2b5517c71e27da6b2139ab958
Edgars Joja <ejoja@mikrotik.com>
2025-05-22 16:30:27 +0300

    Add custom font

--- resources/views/livewire/create-folder.blade.php
+++ resources/views/livewire/create-folder.blade.php
@@ -1,6 +1,6 @@
 <div>
     <x-form wire:submit="save" no-separator>
-        <x-input label="Name" icon="o-folder" wire:model="newDirectoryName" />
+        <x-input label="Folder name" icon="o-folder" wire:model="newDirectoryName" />
 
         <x-slot:actions>
             <x-button label="Create" type="submit" class="btn-primary" spinner="save" />
